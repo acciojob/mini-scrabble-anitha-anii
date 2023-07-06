4a6e84a8f77d98e919a9f0f98073051f2a8872fd
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-06 13:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+let typee =document.getElementById('evaluatedText');
+let countbox =document.getElementById('letterCount');
+        let count=0;
+     typee.addEventListener('keypress',()=>{
+		 	count++;
+		 
+		 countbox.textContent = count;
+	 })
